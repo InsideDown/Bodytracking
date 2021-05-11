1b013d2429c50f1e3d70ead0a96dde1b0c1987da
Paul Mealy <paul@inisdedown.com>
2021-05-11 15:13:23 -0400

    first commit

---
+++ Assets/Scripts/BodyTrackingController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyTrackingController : MonoBehaviour
+{
+
+    public GameObject HeadObj;
+
+    private void Awake()
+    {
+        if (HeadObj == null)
+            throw new System.Exception("A HeadObj must be defined in PaulSceneController");
+    }
+
+    private void OnEnable()
+    {
+        EventManager.OnObjectNewPosition += EventManager_OnObjectNewPosition;
+    }
+
+    private void OnDisable()
+    {
+        EventManager.OnObjectNewPosition -= EventManager_OnObjectNewPosition;
+    }
+
+    private void EventManager_OnObjectNewPosition(string objTypeStr, Transform objectTransform)
+    {
+        TrackObject(objTypeStr, objectTransform);
+    }
+
+    private void TrackObject(string objTypeStr, Transform objectTransform)
+    {
+        string objTrackStr = "type: " + objTypeStr + ", trans: " + objectTransform;
+
+        if (objTypeStr == "head")
+        {
+            HeadObj.transform.position = objectTransform.position;
+            HeadObj.transform.rotation = objectTransform.rotation;
+
+            //RightHandTxt.text = "headPos: " + objectTransform.position;
+        }
+    }
+}
---
+++ Assets/Scripts/PaulControlledRobotController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaulControlledRobotController : MonoBehaviour
+{
+
+    public Transform HeadTransform;
+    //public Transform RightHandTransform;
+    //public Transform LeftHandTransform;
+
+
+    private void Awake()
+    {
+        if (HeadTransform == null)
+            throw new System.Exception("A HeadTransform must be defined in PaulControlledRobotController");
+
+        //if (RightHandTransform == null)
+        //    throw new System.Exception("A RightHandTransform must be defined in PaulControlledRobotController");
+
+        //if (LeftHandTransform == null)
+        //    throw new System.Exception("A LeftHandTransform must be defined in PaulControlledRobotController");
+    }
+
+    private void Update()
+    {
+        Debug.Log("head transform -------");
+        Debug.Log(HeadTransform.position);
+        EventManager.Instance.ObjectNewPosition("head", HeadTransform);
+        //EventManager.Instance.ObjectNewPosition("righthand", RightHandTransform);
+        //EventManager.Instance.ObjectNewPosition("lefthand", LeftHandTransform);
+    }
+}
